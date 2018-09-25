1737f9bcf1b26b506b6f4811ff23492b0d6ae760
buruizhao <buruizhao@163.com>
2018-09-25 21:02:00 +0800

    ‘update

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')), #注册polls应用下的urls.py
+    path('',views.index,name='index'),
+    path('<int:question_id>/', views.detail,name='detail'),
+    path('<int:question_id>/results/', views.results,name='results'),
+    path('<int:question_id>/vote/', views.vote,name='vote'),
+    # path('polls/latest.html', views.index),
+
 ]
